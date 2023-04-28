1a86a85e2200f2eeb7df5f989598b3e283cd30a2
Faneluu <124470285+Faneluu@users.noreply.github.com>
2023-04-28 10:52:44 +0300

    Este Singleton

--- 3_Solution/PlayerTank.h
+++ 3_Solution/PlayerTank.h
@@ -10,10 +10,13 @@ class PlayerTank :public iTank
 private:
     QMediaPlayer* bulletSound;
     PlayerBullet* bullet;
-public:
+    static PlayerTank* instance;
 
     PlayerTank(const int& w,const int& h);
     ~PlayerTank();
+public:
+    static PlayerTank* getInstance(const int& w,const int& h);
+    static void destroyInstance();
 
     void keyPressEvent(QKeyEvent* event);
     void setBulletPos();
